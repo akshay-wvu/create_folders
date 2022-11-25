8f80365cda5f93b62895fdd5c38ce8114236c478
akshay-wvu <119022342+akshay-wvu@users.noreply.github.com>
2022-11-24 22:24:55 -0500

    Update create_folder_v1.0.py

--- create_folder_v1.0.py
+++ create_folder_v1.0.py
@@ -17,9 +17,7 @@ import os
 today = datetime.now()
 
 # create a directory or folder in Linux's "Documents" folder with today's date as its name
-user_name = 'YOUR_LOGIN_USERNAME'
-
-os.mkdir("/home/user_name/Documents/" + today.strftime('%Y%m%d'))
-os.mkdir("/home/user_name/Documents/" + today.strftime('%Y%m%d') + '/' + 'folder_1')                                    # change folder name
-os.mkdir("/home/user_name/Documents/" + today.strftime('%Y%m%d') + '/' + 'folder_1' + '/' + 'folder_1_subfolder_1')     # change subfolder name
-os.mkdir("/home/user_name/Documents/" + today.strftime('%Y%m%d') + '/' + 'folder_1' + '/' + 'folder_1_subfolder_2')     # change subfolder name
+os.mkdir("/home/USER_NAME/Documents/" + today.strftime('%Y%m%d'))
+os.mkdir("/home/USER_NAME/Documents/" + today.strftime('%Y%m%d') + '/' + 'folder_1')                                    # change USER_NAME and folder name
+os.mkdir("/home/USER_NAME/Documents/" + today.strftime('%Y%m%d') + '/' + 'folder_1' + '/' + 'folder_1_subfolder_1')     # change USER_NAME and subfolder name
+os.mkdir("/home/USER_NAME/Documents/" + today.strftime('%Y%m%d') + '/' + 'folder_1' + '/' + 'folder_1_subfolder_2')     # change USER_NAME and subfolder name
